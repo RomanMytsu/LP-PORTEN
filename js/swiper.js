e9d240b4732751888659681af326f5e962d268d1
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2024-05-31 15:44:40 +0300

    add information section

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,8 @@
 const swiper = new Swiper('.swiper', {
   spaceBetween: 30,
+  autoHeight: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
