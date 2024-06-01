c10e65f7cb20c8bae60b62b6b629e1b187e937dd
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2024-06-01 17:49:09 +0300

    add media

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,5 @@
 const swiper = new Swiper('.swiper', {
   spaceBetween: 30,
-  autoHeight: true,
-  centeredSlides: true,
   slidesPerView: "auto",
   navigation: {
     nextEl: '.swiper-button-next',
